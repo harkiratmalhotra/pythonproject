37698a2376209121d7ade8ecd7e4dc28715e65df
harkiratmalhotra <40227521+harkiratmalhotra@users.noreply.github.com>
2023-03-30 23:55:10 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+print(add(3, 4))
+print(subtract(10, 7))
+print(multiply(8, 7))
+print(divide(10, 2))
